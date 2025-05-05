ce3cdc93f5137f4ded96260b746f71762be8ed66
josh <joshua.kan9@gmail.com>
2025-05-05 10:52:26 -0700

    minor changes

--- gui.py
+++ gui.py
@@ -1,5 +1,9 @@
 import os
 import sys
+
+if getattr(sys, "frozen", False):
+    os.environ["PLAYWRIGHT_BROWSERS_PATH"] = os.path.join(sys._MEIPASS, "ms-playwright")
+
 import asyncio
 from pathlib import Path
 from typing import List, Tuple, Dict, Union
